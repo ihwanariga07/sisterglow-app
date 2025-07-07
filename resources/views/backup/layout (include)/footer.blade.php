5960faa568a558aab06e15a75b4119bae1967c3a
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-07-07 23:09:55 +0700

    menambahkan header dan footer

---
+++ resources/views/backup/layout (include)/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="fixed-bottom bg-dark text-white py-2 mt-4 text-center">
+    Copyright &copy; SISTERGLOW
+</footer>
+
+
+
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
