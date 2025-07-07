24ed005f126d6184587db9ab23ca4bd62bfe47b7
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-07-07 22:50:58 +0700

    menambahkan form tambah costumer

---
+++ resources/views/Costumer/form.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Form Tambah Data</div>
+
+                <div class="card-body">
+                    <form method="post" action="/costumer" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nama</label>
+                            <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">No Telepon</label>
+                            <input type="text" name="no_hp" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Email</label>
+                            <input type="email" name="email" class="form-control" id="exampleInputPassword1">
+                        </div>
+
+
+                        <button type="submit" class="btn btn-primary">Tambah</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
