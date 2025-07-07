8e618bbefe8ad588ef5baf290d5bfda889d66f54
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-07-07 23:01:15 +0700

    menambah kan data layanan

--- resources/views/data/layanan.blade.php
+++ resources/views/data/layanan.blade.php
@@ -1,19 +1,23 @@
-
 @extends('layout.master')
-@section('title','Data Costumer')
-@section('MenuCos','active')
+@section('title')
+    Data Layanan
+@endsection
+
+@section('MenuLay')
+    active
+@endsection
 
 @section('konten')
 <div class="container text-center mt-3 bg-white">
-    <h2 class="mb-3">Data Costumer</h2>
+    <h2 class="mb-3">Data layanan</h2>
     <div class="row">
         <div class="m-auto col-6">
             <ol class="list-group">
-                @forelse ($data_cos as $cos)
-                    <li class="list-group-item">{{$costumer}}</li>
+                @forelse ($data_lay as $lay)
+                    <li class="list-group-item">{{$lay}}</li>
                 @empty
                 <div class="alert alert-secondary" role="alert">
-                    Maaf, Data Costumer Tidak Tersedia
+                    Maaf, Data Layanan Tidak Tersedia
                 </div>
                 @endforelse
             </ol>
