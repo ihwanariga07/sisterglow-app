e5c48caf496aa6f5745a7723418a35b90010576d
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-07-13 23:53:26 +0700

    menambahkan template di layouts

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,20 @@ Auth::routes();
 Route::get('/home', function () {
     return view('home');
 });
+
+
+Route::get('/', function () {
+    return view('home');
+})->middleware('auth');
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware('auth');
+
+Route::get('/cektemplate', function () {
+    return view('layouts.template');
+});
+
 // Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 // Route::get('admin', function () {
 //     return view('admin.dashboard');
@@ -22,6 +36,6 @@ Route::get('/home', function () {
 // })->name('admin.dashboard');
 
 // Dashboard WAJIB login
-Route::middleware('auth')->get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->name('admin.dashboard');
+// Route::middleware('auth')->get('/admin/dashboard', function () {
+//     return view('admin.dashboard');
+// })->name('admin.dashboard');
