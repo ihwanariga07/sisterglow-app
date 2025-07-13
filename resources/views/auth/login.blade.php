a1785cdc81d35b62611229ba1b251f98aec6ae6f
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-07-13 23:58:22 +0700

    mengubah guest untuk login cuba

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,79 @@
+@extends('layouts.guest')
+@section('title','Login | Cuba Admin')
 
-@extends('layout.guest')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center align-items-center min-vh-100">
-        <div class="col-md-6">
-            <div class="card shadow border-0">
-                <div class="card-header bg-primary text-white text-center">
-                    <h4 class="mb-0">{{ __('Login to Your Account') }}</h4>
+<div class="container-fluid p-0">
+  <div class="row m-0">
+    <div class="col-12 p-0">
+      <div class="login-card login-dark">
+        <div>
+
+          <div class="text-center mb-4">
+            <a class="logo" href="{{ url('/') }}">
+              <img class="img-fluid for-light" src="{{ asset('cuba-assets/images/logo/logo.png') }}">
+              <img class="img-fluid for-dark" src="{{ asset('cuba-assets/images/logo/logo_dark.png') }}">
+            </a>
+          </div>
+
+          <div class="login-main">
+            <form class="theme-form" method="POST" action="{{ route('login') }}">
+              @csrf
+              <h4>Sign in to account</h4>
+              <p>Enter your email & password to login</p>
+
+              {{-- Email --}}
+              <div class="form-group">
+                <label>Email Address</label>
+                <input id="email" type="email"
+                       class="form-control @error('email') is-invalid @enderror"
+                       name="email" value="{{ old('email') }}" required autofocus>
+                @error('email') <small class="text-danger">{{ $message }}</small> @enderror
+              </div>
+
+              {{-- Password --}}
+              <div class="form-group">
+                <label>Password</label>
+                <div class="form-input position-relative">
+                  <input id="password" type="password"
+                         class="form-control @error('password') is-invalid @enderror"
+                         name="password" required>
+                  <div class="show-hide"><span class="show"></span></div>
+                  @error('password') <small class="text-danger">{{ $message }}</small> @enderror
                 </div>
-                <div class="card-body p-4">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+              </div>
 
-                        {{-- Email --}}
-                        <div class="mb-3">
-                            <label for="email" class="form-label">{{ __('Email Address') }}</label>
-                            <input id="email" type="email"
-                                   class="form-control @error('email') is-invalid @enderror"
-                                   name="email" value="{{ old('email') }}" required autofocus>
-                            @error('email')
-                                <div class="text-danger small mt-1">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                        {{-- Password --}}
-                        <div class="mb-3">
-                            <label for="password" class="form-label">{{ __('Password') }}</label>
-                            <input id="password" type="password"
-                                   class="form-control @error('password') is-invalid @enderror"
-                                   name="password" required>
-                            @error('password')
-                                <div class="text-danger small mt-1">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                        {{-- Remember Me --}}
-                        <div class="mb-3 form-check">
-                            <input class="form-check-input" type="checkbox" name="remember"
-                                   id="remember" {{ old('remember') ? 'checked' : '' }}>
-                            <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
-                        </div>
-
-                        {{-- Submit --}}
-                        <div class="d-grid">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Login') }}
-                            </button>
-                        </div>
-
-                        {{-- Forgot Password --}}
-                        @if (Route::has('password.request'))
-                            <div class="text-center mt-3">
-                                <a class="text-decoration-none" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        @endif
-
-                        {{-- Register link --}}
-                        @if (Route::has('register'))
-                            <div class="text-center mt-2">
-                                <a class="text-decoration-none" href="{{ route('register') }}">
-                                    {{ __('Belum punya akun? Daftar') }}
-                                </a>
-                            </div>
-                        @endif
-                    </form>
+              {{-- Ingat Saya + Submit --}}
+              <div class="form-group mb-0">
+                <div class="form-check">
+                  <input class="checkbox-primary form-check-input" id="remember_me" type="checkbox" name="remember">
+                  <label class="text-muted form-check-label" for="remember_me">Remember password</label>
                 </div>
-            </div>
+                <a class="link" href="{{ route('password.request') }}">Forgot password?</a>
+                <div class="text-end">
+                  <button class="btn btn-primary btn-block w-100 mt-3" type="submit">Sign in</button>
+                </div>
+              </div>
+
+              <h6 class="text-muted mt-4 or">Or Sign in with</h6>
+              <div class="social mt-4">
+                <div class="btn-showcase">
+                  <a class="btn btn-light" href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                  <a class="btn btn-light" href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                  <a class="btn btn-light" href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                  <a class="btn btn-light" href="#"><i class="fa-brands fa-google"></i></a>
+                </div>
+              </div>
+
+              <p class="mt-4 mb-0 text-center">
+                Don't have account?
+                <a class="ms-2" href="{{ route('register') }}">Create Account</a>
+              </p>
+            </form>
+          </div>
+
         </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -2,17 +2,35 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>@yield('title','Login | Cuba Admin')</title>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Rubik:400,500,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
+
+    <!-- Vendor & theme CSS (kopi dari template Cuba) -->
+    <link rel="stylesheet" href="{{ asset('cuba-assets/css/vendors/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('cuba-assets/css/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('cuba-assets/css/vendors/icofont.css') }}">
+    <link rel="stylesheet" href="{{ asset('cuba-assets/css/vendors/feather-icon.css') }}">
+    <link rel="stylesheet" href="{{ asset('cuba-assets/css/style.css') }}">
+    <link id="color" rel="stylesheet" href="{{ asset('cuba-assets/css/color-1.css') }}">
+    <link rel="stylesheet" href="{{ asset('cuba-assets/css/responsive.css') }}">
+
+    @stack('styles')
 </head>
-<body>
-    <div class="container">
-        @yield('content')
-    </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<body class="dark-only"> {{-- “login-dark” kelas khas Cuba --}}
+    @yield('content')
+
+    <!-- Vendor & theme JS -->
+    <script src="{{ asset('cuba-assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('cuba-assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('cuba-assets/js/icons/feather-icon/feather.min.js') }}"></script>
+    <script src="{{ asset('cuba-assets/js/icons/feather-icon/feather-icon.js') }}"></script>
+    <script src="{{ asset('cuba-assets/js/config.js') }}"></script>
+    <script src="{{ asset('cuba-assets/js/script.js') }}"></script>
+
+    @stack('scripts')
 </body>
 </html>
