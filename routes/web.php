5b20c626cb674899fdc13c48e1738429aa8b819f
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-07-14 00:27:35 +0700

    menhapus semua template

--- routes/web.php
+++ routes/web.php
@@ -12,30 +12,8 @@ Auth::routes();
 Route::get('/home', function () {
     return view('home');
 });
-
-
-Route::get('/', function () {
-    return view('home');
-})->middleware('auth');
-
-Route::get('/home', function () {
-    return view('home');
-})->middleware('auth');
-
-Route::get('/cektemplate', function () {
-    return view('layouts.template');
+Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+Route::get('admin', function () {
+    return view('admin.dashboard');
 });
 
-// Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-// Route::get('admin', function () {
-//     return view('admin.dashboard');
-// });
-//tanpa login
-// Route::get('/admin/dashboard', function () {
-//     return view('admin.dashboard');
-// })->name('admin.dashboard');
-
-// Dashboard WAJIB login
-// Route::middleware('auth')->get('/admin/dashboard', function () {
-//     return view('admin.dashboard');
-// })->name('admin.dashboard');
