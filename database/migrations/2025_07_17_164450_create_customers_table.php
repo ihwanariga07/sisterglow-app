66d9a23460c0dba7d6de25be74732685ec9d8d29
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-07-17 23:46:21 +0700

    mengubah isi migrationcostumer

--- database/migrations/2025_07_17_164450_create_customers_table.php
+++ database/migrations/2025_07_17_164450_create_customers_table.php
@@ -4,22 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('no_hp');
+            $table->string('email')->unique();
+            $table->string('foto')->nullable(); // nama file foto
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('customers');
